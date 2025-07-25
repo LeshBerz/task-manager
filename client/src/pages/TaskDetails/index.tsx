96af54f8443374de30c8c3c652390660785d7f63
LeshiyAda9H <114980114+LeshiyAda9H@users.noreply.github.com>
2025-07-25 21:12:32 +0300

    feat: add TaskDetails page

---
+++ client/src/pages/TaskDetails/index.tsx
@@ -0,0 +1,10 @@
+import TaskForm from '@features/task-management/components/TaskForm';
+
+/**
+ * Page for editing a task
+ */
+const TaskDetails: React.FC = () => {
+  return <TaskForm />;
+};
+
+export default TaskDetails;
