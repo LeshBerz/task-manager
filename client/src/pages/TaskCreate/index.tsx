d1cc61ec19d5056238559a1d15dc2adcaba07acd
LeshiyAda9H <114980114+LeshiyAda9H@users.noreply.github.com>
2025-07-25 21:11:56 +0300

    feat: add TaskCreate page

---
+++ client/src/pages/TaskCreate/index.tsx
@@ -0,0 +1,10 @@
+import TaskForm from '@features/task-management/components/TaskForm';
+
+/**
+ * Page for creating a new task
+ */
+const TaskCreate: React.FC = () => {
+  return <TaskForm />;
+};
+
+export default TaskCreate;
