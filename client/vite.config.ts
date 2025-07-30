ebcccb317c7cdca3c7e8abb85e7444ff10a3b802
LeshiyAda9H <114980114+LeshiyAda9H@users.noreply.github.com>
2025-07-30 17:23:25 +0300

    fix: alias & routes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,11 +7,11 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@app': path.resolve(__dirname, 'src/app'),
+      '@app': path.resolve(__dirname, './src/app'),
       '@entities': path.resolve(__dirname, './src/entities'),
       '@features': path.resolve(__dirname, './src/features'),
       '@shared': path.resolve(__dirname, './src/shared'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@pages': path.resolve(__dirname, './src/pages'),
     },
   },
 });
